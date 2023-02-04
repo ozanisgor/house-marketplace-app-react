c7d5b33e597d19f563904fcf02bd8dc1ff9b9d57
ozan isgor <ozanisgor11@gmail.com>
2023-02-04 20:58:16 +0300

    add: save user to firestore

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,6 +5,7 @@ import {
   createUserWithEmailAndPassword,
   updateProfile
 } from 'firebase/auth'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
@@ -30,17 +31,28 @@ function SignUp() {
 
   const onSubmit = async e => {
     e.preventDefault()
+
     try {
       const auth = getAuth()
+
       const userCredentials = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       )
+
       const user = userCredentials.user
+
       updateProfile(auth.currentUser, {
         displayName: name
       })
+
+      const formDataCopy = { ...formData }
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
       navigate('/')
     } catch (error) {
       console.log(error)
