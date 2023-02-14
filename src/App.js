b59d2cf5a47aa387127ad51a3b390ce507fc3c73
ozan isgor <ozanisgor11@gmail.com>
2023-02-14 14:50:13 +0300

    add: Create Listing Page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Category from './pages/Category'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import CreateListing from './pages/CreateListing'
 
 function App() {
   return (
@@ -50,6 +51,10 @@ function App() {
           <Route
             path="/forgot-password"
             element={<ForgotPassword />}
+          />
+          <Route
+            path="/create-listing"
+            element={<CreateListing />}
           />
         </Routes>
         <Navbar />
