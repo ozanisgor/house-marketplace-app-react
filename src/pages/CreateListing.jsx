724714d7221259456f465cf487591fdad4bf9d93
ozan isgor <ozanisgor11@gmail.com>
2023-02-16 17:52:31 +0300

    fix: formatted address instability

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -111,7 +111,6 @@ function CreateListing() {
     } else {
       geolocation.lat = latitude
       geolocation.lng = longitude
-      location = address
     }
 
     const storeImage = async image => {
@@ -163,9 +162,9 @@ function CreateListing() {
       timestamp: serverTimestamp()
     }
 
+    formDataCopy.location = address
     delete formDataCopy.images
     delete formDataCopy.address
-    location && (formDataCopy.location = location)
     !formDataCopy.offer && delete formDataCopy.discountedPrice
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
